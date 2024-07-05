71255633d032e4ada8a6f16c8bd7e062f510c89c
AnhQuan <quan.nguyen@htecom.vn>
2024-07-05 21:47:56 +0700

    build front end web

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -4,15 +4,17 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <title>{{ $title ?? 'AAAAAAAAAAA' }}</title>
+        <title>{{ $title ?? 'Banana Store Badminton' }}</title>
         @vite(['resources/css/app.css', 'resource/js/app.js'])
         @livewireStyles
     </head>
     <body class="bg-slate-200 dark:bg-slate-700">
+        @livewire('partials.navbar')
         <main>
             {{ $slot }}
         </main>
-
+        @livewire('partials.footer')
+        
         @livewireScripts
     </body>
 </html>
